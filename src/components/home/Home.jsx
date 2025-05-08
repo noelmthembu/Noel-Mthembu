fbe2023fa05440819bc0450eb20d89b2d4c8dcf1
Noel Mthembu <109946871+noelmthembu@users.noreply.github.com>
2025-05-08 15:46:39 +0200

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./home.css";
 import Me from "../../assets/profile2.jpeg";
 import HeaderSocials from './HeaderSocials';
 import ScrollDown from './ScrollDown';
 
-const Home = () =>{
-    return (
-        <section className="home container" id="home">
-          <div className="intro">
-            <img src={Me} alt="" className="home__img" />
-            <h1 className="home__name">Noel Mthembu</h1>
-            <span className="home__education">I'm a Software Developer</span>
-            <HeaderSocials/>
-            <a href="#contact" className="btn">Hire Me</a>
-            <ScrollDown/>
-          </div>
-        
-        </section>
-    );
-}
+const Home = () => {
+  const [currentText, setCurrentText] = useState(0);
+  const texts = ["I'm a Software Developer", "I'm a Youtuber", "I'm an Artist"];
+  
+  useEffect(() => {
+    const typingInterval = setInterval(() => {
+      setCurrentText((prevText) => (prevText + 1) % texts.length);
+    }, 4000); // Change every 4 seconds
+
+    return () => clearInterval(typingInterval);
+  }, [texts.length]);
+
+  return (
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={Me} alt="" className="home__img" />
+        <h1 className="home__name">Noel Mthembu</h1>
+        <span className="home__education">{texts[currentText]}</span>
+        <HeaderSocials />
+        <a href="#contact" className="btn">Hire Me</a>
+        <ScrollDown />
+      </div>
+    </section>
+  );
+};
+
 export default Home;
