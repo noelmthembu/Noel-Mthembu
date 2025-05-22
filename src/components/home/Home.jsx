1fcef9285654f337d97db3ba43d39beddfed10c7
Noel Mthembu <109946871+noelmthembu@users.noreply.github.com>
2025-05-22 11:08:03 +0200

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <section className="home container" id="home">
       <div className="intro">
         <img src={Me} alt="Profile" className="home__img" />
-        <h1 className="home__name">Noel Mthembu</h1>
+        <h1 className="home__name">Noel Mthembu <i class="fa-solid fa-code"></i></h1>
         <span className="home__education">
           I'm a Software Developer <i className="fa-solid fa-code"></i>
         </span>
