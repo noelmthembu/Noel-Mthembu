4e005a3d31addbdf64e561a510a194e7a82b5d01
Noel Mthembu <109946871+noelmthembu@users.noreply.github.com>
2025-05-22 11:06:33 +0200

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <img src={Me} alt="Profile" className="home__img" />
         <h1 className="home__name">Noel Mthembu</h1>
         <span className="home__education">
-          I'm a Software Developer
+          I'm a Software Developer <i className="fa-solid fa-code"></i>
         </span>
         <HeaderSocials />
         <a href="#contact" className="btn">Hire Me</a>
